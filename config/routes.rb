20cb0688feecea7ed7d88d96414e8a0adfd1b803
halkity <saito75061@gmail.com>
2020-08-06 19:54:44 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  omniauth_callbacks: 'users/omniauth_callbacks',
+  registrations: 'users/registrations'
+}
   root 'users#index'  # ログインor新規登録を選ぶページ
   resources :users, only: :new  # 新規登録方法を選ぶページ
 end
