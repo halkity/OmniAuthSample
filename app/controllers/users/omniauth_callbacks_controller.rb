c22113f1965919173a123e8ac811410cd7cba604
halkity <saito75061@gmail.com>
2020-08-07 15:00:52 +0900

    ＠sns_idという変数を作り、新規登録画面で扱えるようにする

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -24,8 +24,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     # ＠userが保存済みで無い場合(新規登録する場合)、＠userという変数をそのまま新規登録のviewsで利用するためにrenderを使用する。
     if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
     else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+      @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end
   end
