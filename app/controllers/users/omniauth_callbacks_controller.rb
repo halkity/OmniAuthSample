7298ee91654b1517eb07c67fb8b6916439840f14
halkity <saito75061@gmail.com>
2020-08-07 17:05:44 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -21,7 +21,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # DB操作を行うメソッドUser.from_omniauthを仮で作り、変数を渡す。
     sns_info = User.from_omniauth(request.env["omniauth.auth"])
     @user = sns_info[:user]
-
     # ＠userが保存済みで無い場合(新規登録する場合)、＠userという変数をそのまま新規登録のviewsで利用するためにrenderを使用する。
     if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
