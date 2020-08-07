ae16f2f561a1ec740edf799878e50e4acb2fc231
halkity <saito75061@gmail.com>
2020-08-07 17:07:57 +0900

    first_or_createをfirst_or_initializeに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+        :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
   has_many :sns_credentials
 
-
-  # SNS情報未登録
-  # 新規登録→SNSの情報を取得し、併せてユーザー情報を登録
-  # emailが合致するとき→ログインへ
-
-  # SNS情報登録済み
-  # そのままログインへ
-
-  def self.from_omniauth(auth)  # authにはAPIから受け取ったレスポンスを代入
-    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_initialize
     # sns認証したことがあればアソシエーションで取得
     # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = sns.user || User.where(email: auth.info.email).first_or_initialize(
@@ -29,7 +19,6 @@ class User < ApplicationRecord
     end
     { user: user, sns: sns }
   end
-
 
 end
   
